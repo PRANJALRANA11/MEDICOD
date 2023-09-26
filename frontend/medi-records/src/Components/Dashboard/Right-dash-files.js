70166de498b389df766e8dedd8a9345ba566943c
PRANJALRANA11 <pranajlrana1235@gmail.com>
2023-09-26 09:49:11 +0530

    adding-props
    Signed-off-by: PRANJALRANA11 <pranajlrana1235@gmail.com>

--- frontend/medi-records/src/Components/Dashboard/Right-dash-files.js
+++ frontend/medi-records/src/Components/Dashboard/Right-dash-files.js
@@ -1,88 +1,74 @@
-import React from 'react'
+import React from 'react';
 import '../../Styles/Dash-styles/Right_files.css';
 
-export default function Right_dash_files() {
+export default function Right_dash_files(props) {
+
+  const report_data = [
+    {
+      reportName: "Report A",
+      clinicName: "Clinic A",
+      uploadedPhoto: "https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png",
+      uploadDate: "Saved On 23 June 2023",
+      appointmentDate: "Appointment On 23 June 2023"
+    },
+    {
+      reportName: "Report A",
+      clinicName: "Clinic A",
+      uploadedPhoto: "https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png",
+      uploadDate: "Saved On 23 June 2023",
+      appointmentDate: "Appointment On 23 June 2023"
+    },
+    {
+      reportName: "Report A",
+      clinicName: "Clinic A",
+      uploadedPhoto: "https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png",
+      uploadDate: "Saved On 23 June 2023",
+      appointmentDate: "Appointment On 23 June 2023"
+    },
+    {
+      reportName: "Report A",
+      clinicName: "Clinic A",
+      uploadedPhoto: "https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png",
+      uploadDate: "Saved On 23 June 2023",
+      appointmentDate: "Appointment On 23 June 2023"
+    },
+    {
+      reportName: "Report A",
+      clinicName: "Clinic A",
+      uploadedPhoto: "https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png",
+      uploadDate: "Saved On 23 June 2023",
+      appointmentDate: "Appointment On 23 June 2023"
+    },
+    {
+      reportName: "Report A",
+      clinicName: "Clinic A",
+      uploadedPhoto: "https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png",
+      uploadDate: "Saved On 23 June 2023",
+      appointmentDate: "Appointment On 23 June 2023"
+    },
+  
+  ]
   return (
     <div>
-    <div className='file_status'>Saved files</div>
-    <div className='files'> 
-     <div className='report'>
-        <div className='report_name'>
-             <p>Report A</p>       
-        </div>
-      <div className='report_image'>
-        <img src='https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png'alt='report'/>
+      <div className='file_status'>{props.state_files}</div>
+      <div className='files'>
+      {report_data.map((report, index) => (
+        
+          <div className='report'  key={index}>
+            <div className='report_name'>
+              <p>{report.reportName}</p>
+            </div>
+            <div className='report_image'>
+              <img src={report.uploadedPhoto} alt='report' />
+            </div>
+            <div className='Report_details'>
+              <div className='clinic_name'>{report.clinicName}</div>
+              <div className='upload_date'>{report.uploadDate}</div>
+              <div className='appointments_date'>{report.appointmentDate}</div>
+            </div>
+          </div>
+      ))}
       </div>
-      <div className='Report_details'> 
-      <div className='clinic_name'>
-        Clinic A
-      </div>
-      <div className='upload_date'>
-        Saved On 23 June 2023
-      </div>
-      <div className='appointments_date'>
-        Appointment On 23 June 2023
-      </div>
-      </div>
-      </div>
-     <div>
-        <div className='report_name'>
-             <p>Report A</p>       
-        </div>
-      <div className='report_image'>
-        <img src='https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png'alt='report'/>
-      </div>
-      <div className='Report_details'> 
-      <div className='clinic_name'>
-        Clinic A
-      </div>
-      <div className='upload_date'>
-        Saved On 23 June 2023
-      </div>
-      <div className='appointments_date'>
-        Appointment On 23 June 2023
-      </div>
-      </div>
-      </div>
-     <div>
-        <div className='report_name'>
-             <p>Report A</p>       
-        </div>
-      <div className='report_image'>
-        <img src='https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png'alt='report'/>
-      </div>
-      <div className='Report_details'> 
-      <div className='clinic_name'>
-        Clinic A
-      </div>
-      <div className='upload_date'>
-        Saved On 23 June 2023
-      </div>
-      <div className='appointments_date'>
-        Appointment On 23 June 2023
-      </div>
-      </div>
-      </div>
-     <div>
-        <div className='report_name'>
-             <p>Report A</p>       
-        </div>
-      <div className='report_image'>
-        <img src='https://i.pinimg.com/originals/da/2e/db/da2edbf7c34fd73ed79784235e27aa29.png'alt='report'/>
-      </div>
-      <div className='Report_details'> 
-      <div className='clinic_name'>
-        Clinic A
-      </div>
-      <div className='upload_date'>
-        Saved On 23 June 2023
-      </div>
-      <div className='appointments_date'>
-        Appointment On 23 June 2023
-      </div>
-      </div>
-      </div>
-      </div>
-      </div>
-  )
+    </div>
+  );
 }
--- frontend/medi-records/src/Pages/Dashboard.js
+++ frontend/medi-records/src/Pages/Dashboard.js
@@ -3,7 +3,6 @@ import Navbar_Dash from '../Components/Dashboard/Navbar_Dash'
 import { Route, Routes } from 'react-router-dom';
 import Right_Dash from '../Components/Dashboard/Right_Dash';
 import Main from '../Components/Dashboard/Main';
-import Right_dash_files from '../Components/Dashboard/Right-dash-files';
 export default function Dashboard() {
   return (
     <div>
@@ -11,7 +10,6 @@ export default function Dashboard() {
       <Route path="/Dashboard" element={<>
         <Navbar_Dash/>
         {/* <Main/> */}
-        <Right_dash_files/>
         <Right_Dash/>
       </>} />
         </Routes>
